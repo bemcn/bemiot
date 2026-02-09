1a20823caee459d8be4e9d2cb5b7520d2602c049
Bem <tech@bemcn.com>
2026-02-09 17:02:35 +0800

    Initial commit

--- src/main/java/org/bem/iot/mapper/postgresql/MonitorSystemMapper.java
+++
@@ -1,9 +0,0 @@
-package org.bem.iot.mapper.postgresql;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.bem.iot.model.monitor.MonitorSystem;
-
-@Mapper
-public interface MonitorSystemMapper extends BaseMapper<MonitorSystem> {
-}
